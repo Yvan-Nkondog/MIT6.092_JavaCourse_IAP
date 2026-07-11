1541f988b2bea4ac2c64f975ed31af394d73be56
Yvan Nkondog <yvan.nkondog@protonmail.com>
2026-07-11 19:26:16 -0400

    Update of 'Assignement3/Marathon.java' to implement bubble sort algorithm.

--- ASSIGNMENTS/Assignment3/Marathon.java
+++ ASSIGNMENTS/Assignment3/Marathon.java
@@ -1,5 +1,4 @@
 package Chap3;
-import java.util.Arrays;
 
 // A group of friends participate in the Boston Marathon.
 // Find the best performer.
@@ -19,34 +18,39 @@ public class Marathon {
 				"Justin", "Mark", "Mike", "Rebecca", "Adalbert"
 		};
 		
-		// Performance in hours 
+		// Array storing the performance in hours 
 		double[] performance = {
 				25.4, 29.5, 20.3, 15.2, 27.5, 
 				20.2, 23.3, 22.5, 32.1, 20.9
 		};
 		
-		// Construct a deep copy of the performance vector and
-		// sort the copy.
-		double[] performanceCopy = performance.clone();
-		Arrays.sort(performanceCopy);
-		
-		double bestPerformance = performanceCopy[0];
-		double secondBestPerformance = performanceCopy[1];
-		String bestParticipant = "";
-		String secondBestParticipant = "";
-		
-		for (int i = 0; i < performance.length; i++) {
-			if (performance[i] == bestPerformance) {
-				bestParticipant = participants[i];
+		// Use the bubble sort algorithm in order to sort the
+		// to sort both arrays at the same time.
+		double temp_performance = 0.0;
+		String temp_participant = "";
+		for (int i = performance.length - 1; i >1 ; i--) {
+			for (int j = 0; j < i - 1; j++) {
+				if (performance[j + 1] < performance[j]) {
+					// Reverse the values at adjacent positions 
+					// when the condition applies.
+					temp_performance = performance[j];
+					performance[j] = performance[j + 1];
+					performance[j + 1] = temp_performance;
+					
+					temp_participant = participants[j];
+					participants[j] = participants[j + 1];
+					participants[j + 1] = temp_participant;
+				}	
 			}
-			if (performance[i] == secondBestPerformance) {
-				secondBestParticipant = participants[i];
-			}
-		}
+		}  // End of bubble sort algorithm (both arrays are sorted).
 		
-		System.out.println("Best participant : " + bestParticipant 
-				+ " ; Best time : " + bestPerformance);
-		System.out.println("Second best participant : " + secondBestParticipant 
-				+ " ; Second best time : " + secondBestPerformance);
+		
+		// From the sorted arrays, extract the best performance (and participant)
+		// and the second best performance (participant), positions 0 and 1. 
+		// Then print the results.
+		System.out.println("Best participant : " + participants[0] 
+				+ " ; Best time : " + performance[0]);
+		System.out.println("Second best participant : " + participants[1] 
+				+ " ; Second best time : " + performance[1]);
 	}
 }
